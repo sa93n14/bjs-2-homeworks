134ab5591423f84fd81b80b80e7cc0722cceda96
Mal1n14 <alexander1993belov@yandex.ru>
2023-01-09 16:55:34 +0300

    initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
 function compareArrays(arr1, arr2) {
   
+  return arr1.every((item, index) => item === arr2[index]) && arr1.length === arr2.length;  
+  
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = 
+  users.filter(item => item.gender === gender).map(item => item.age).reduce((acc, item, index, users) => {
+    acc += item;
+    if (index === users.length - 1) {
+      return acc / users.length;
+    }
+    return acc;
+  }, 0);
+  return result;
 }
