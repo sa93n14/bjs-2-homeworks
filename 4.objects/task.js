05585854e05ce7fb3428df58faa26c02f7b97128
Mal1n14 <alexander1993belov@yandex.ru>
2023-01-19 19:50:56 +0300

    initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,44 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.marks !== undefined) {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  let result;
+
+  if (this.marks === undefined || this.marks.length === 0) {
+    return 0;
+  } else {
+    result = this.marks.reduce((acc, item, index, marks) => {
+        acc += item;
+        if (index === marks.length - 1) {
+            return acc / marks.length;
+        }
+        return acc;
+    }, 0);
+  }
+  return result;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
 }
+
+
+// "marks" in Student === true
+// Student.hasOwnProperty("marks") === false
