1976a4d97a21d80124dc22daed8be8e04ef5a98a
Mal1n14 <alexander1993belov@yandex.ru>
2023-02-07 19:09:45 +0300

    initial commit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,46 @@
-﻿
+﻿function parseCount(number) {
+    let result = Number.parseFloat(number);
+    if (!result) {
+        throw new Error("Невалидное значение");
+    } 
+    return result;
+}
+
+function validateCount(number) {
+    try {
+        return parseCount(number);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) { 
+        this.a = a;
+        this.b = b;
+        this.c = c;   
+
+        if ((a + b) < c || (a + c) < b || (b + c) < a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        let p = 0.5 * (this.a + this.b + this.c)
+        return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3))
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        new Error(perimeter, area);
+    }
+    // В случае перехвата исключения возвращайте объект с двумя геттерами area и perimeter, которые возвращают строку: «Ошибка! Треугольник не существует».
+    // не понимаю что надо сделать 
+}
