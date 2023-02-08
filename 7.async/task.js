496c9cb5866bee58e82796382cc88c1d77703bd9
Mal1n14 <alexander1993belov@yandex.ru>
2023-02-08 23:28:39 +0300

    initial commit

---
+++ 7.async/task.js
@@ -0,0 +1,55 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = []; 
+        this.intervalId = null; 
+    }
+
+    addClock(time, callback) {
+        if (!time || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        }
+        
+        if (this.alarmCollection.find(clockTime => clockTime === time)) {
+            console.warn('Уже присутствует звонок на это же время');
+        }
+
+        this.alarmCollection.push({callback: callback, time: time, canCall: true});
+    } 
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter((Clock) => Clock.time !== time);
+    } 
+
+    getCurrentFormattedTime() {
+        return new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    } 
+
+    start() {
+        if (this.intervalId !== null) {
+            return;
+        }
+
+        this.intervalId = setInterval(this.alarmCollection.forEach(element => {
+            if (element.time === this.getCurrentFormattedTime()) {
+                    element.canCall = false;
+                    element.callback();
+                }
+            }
+                
+        ), 1000)
+    } 
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    } 
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(element => element.canCall = true)
+    } 
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    } 
+}
